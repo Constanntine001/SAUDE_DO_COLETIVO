4fb294519c2ea07f30c170f7fc80e557a863f293
Constanntine <84543782+Constanntine001@users.noreply.github.com>
2022-10-31 19:12:01 -0300

    Add joystick do player

--- Godot-Analog-Stick-1.0/README.md
+++
@@ -1,6 +0,0 @@
-# Godot-Analog-Stick
-Godot Analog stick suitable for using in games on mobile devices and tablets (all touch screen devices)
-
-made by Jakub Grzesik (http://www.kivano.games/)
-ported to v 3.1 and example by (Mohamed Sami Hadef)
-permission by Jakub Grzesik
---
+++ Mapa/Player.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+export var pSpeed = 5;
+export(NodePath) var _analogPath
+var analogNode
+
+func _ready():
+	analogNode = get_node(_analogPath)
+	pass
+
+func _process(delta):
+	var analogForce = analogNode.get_force()
+	
+	position.x += pSpeed * analogForce.x
+	position.y -= pSpeed * analogForce.y
+	
+	pass
