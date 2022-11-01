f4ec9c7c9ab69a5afc9472f88dae26a5067970de
Constanntine <84543782+Constanntine001@users.noreply.github.com>
2022-10-31 23:32:26 -0300

    Ultimo commit

--- MAPA_PRINCIPAL/Panel.gd
+++ MAPA_PRINCIPAL/Panel.gd
@@ -7,6 +7,8 @@ func _ready():
 
 func _on_Area2D_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	visible = true
+	var dialog = Dialogic.start("test")
+	add_child(dialog)
 	pass # Replace with function body.
 
 
