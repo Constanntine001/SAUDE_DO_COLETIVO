761b74b29fa563f15f69f92217462331a88481c9
Constanntine <gabrielcegantini@hotmail.com>
2022-12-03 19:36:37 -0300

    NewUpdate

--- MAPA_PRINCIPAL/Player.gd
+++ MAPA_PRINCIPAL/Player.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends AnimatedSprite
 
 export var pSpeed = 5;
 export(NodePath) var _analogPath
@@ -10,6 +10,17 @@ func _ready():
 
 func _process(delta):
 	var analogForce = analogNode.get_force()
+	
+	if analogForce.x == 0 || analogForce.y == 0:
+		play("IDLE")
+	elif analogForce.x < 0 && (abs(analogForce.x) > abs(analogForce.y)):
+		play("WALK_WEST")
+	elif analogForce.x > 0 && (abs(analogForce.x) > abs(analogForce.y)):
+		play("WALK_EAST")
+	elif analogForce.y < 0 && (abs(analogForce.y) > abs(analogForce.x)):
+		play("WALK_SOUTH")
+	elif analogForce.y > 0 && (abs(analogForce.y) > abs(analogForce.x)):
+		play("WALK_NORTH")
 	
 	position.x += pSpeed * analogForce.x
 	position.y -= pSpeed * analogForce.y
