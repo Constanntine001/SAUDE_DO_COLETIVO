ac94803a82f3b3d556e7d4a22075656f7d9e1053
Constanntine <84543782+Constanntine001@users.noreply.github.com>
2023-01-23 20:03:39 -0300

    Update

--- asset_credits.gd
+++ asset_credits.gd
@@ -4,5 +4,6 @@ extends Node
 
 # Super_Retro_World_Interior_pack_week42 
 # https://noranekogames.itch.io/hana?download
+# https://noranekogames.itch.io/kana
 # https://godotengine.org/asset-library/asset/318 Analog
 # https://forums.rpgmakerweb.com/index.php?threads/bigger-and-better-grannys-modern-tiles-sprites-for-mv.43858/
